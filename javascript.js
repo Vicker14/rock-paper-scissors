1d917155908e592f04f548357517847142034c3b
Vicker <vcalero2001@gmail.com>
2025-04-24 02:04:18 +0200

    Human choice logic added

--- javascript.js
+++ javascript.js
@@ -7,3 +7,27 @@ function getComputerChoice() {
     else if (indexValue === 2)
         return "scissors";
 }
+
+function getHumanChoice() {
+    const humanChoice = prompt("Enter your choice (rock, paper, scissors): ");
+
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+        return humanChoice;
+
+    } else {
+        // This is a recursive call to getHumanChoice if the input is invalid
+        // It will keep asking for input until a valid choice is made
+
+        alert("Invalid choice! Please enter rock, paper, or scissors.");
+        return getHumanChoice();
+    }
+}
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(){
+    const humanChoice = getHumanChoice().toLowerCase();
+    const computerChoice = getComputerChoice();
+
+
+}
